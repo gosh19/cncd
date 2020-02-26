042ee56820a6301fc9152742e927a97be8e8fb24
gosh19 <matiascianci@gmail.com>
2020-02-26 09:43:42 -0300

    todo

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CNCD</title>
+
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Estilos -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+
+  </head>
+  <body>
+    <header>
+    <img src="img/Foto_inicio.jpg" alt="Casa Rosada" class="img-fluid">
+  </header>
+
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto mt-5">
+        <img src="img/estudio.jpg" alt="Estudio" class="img-fluid">
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row justify-content-center text-center mt-5">
+      <h1 class="Lalezar texto-azul">Aseguramos la calidad</h1>
+    </div>
+    <div class="row justify-content-center">
+      <div class="w-60 col-xl-6 col-lg-6">
+        <p class="text-center  Lato-light texto-violeta">Tras años de esta nueva metodología de estudio asentada en nuestro país, hemos brindado a nuestros ciudadanos, para la seguridad y tranquilidad de todos,un ente regulador de capacitaciones on-line. De esta manera nos aseguramos una mejor calidad de estudio. </p>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="condiciones p-4 container-fluid">
+    <div class="container">
+      <div class="row mt-5 justify-content-center">
+        <h1 class="Lalezar text-center text-white titulo w-75">Las instituciones cumplen con el siguente régimen</h1>
+      </div>
+      <div class="row mt-5">
+        <!-- NUMEROS -->
+        <div class="d-flex align-items-center mx-auto">
+          <div class="col-lg-3 col-md-12">
+            <div class="circulo-numero mx-auto">
+              <h1 class="text-center text-white ">1</h1>
+            </div>
+          </div>
+        </div>
+        <!-- CONDICIONES -->
+        <div class="col-lg-9 col-md-12 mx-auto mt-5">
+          <p>A los efectos de la presente reglamentación, se entiende por Educación a Distancia la opción pedagógica y didáctica donde la relación docente-alumno se encuentra separada en Sistema Argentino de Información Jurídica el tiempo y/o en el espacio, durante todo o gran parte del proceso educativo, en el marco de una estrategia pedagógica integral que utiliza soportes materiales y recursos tecnológicos, tecnologías de la información y la comunicación, diseñados especialmente para que los/as alumnos/as alcancen los objetivos de la propuesta educativa.
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <!-- NUMEROS -->
+        <div class="d-flex align-items-center mx-auto">
+          <div class="col-lg-3 col-md-12">
+            <div class="circulo-numero mx-auto my-start">
+              <h1 class="text-center text-white">2</h1>
+            </div>
+          </div>
+        </div>
+        <!-- CONDICIONES -->
+        <div class="col-lg-9 col-md-12 mx-auto mt-5">
+          <p>Asimismo, se entiende que quedan comprendidos en la denominación Educación a Distancia los estudios conocidos como educación semipresencial, educación asistida, educación abierta, educación virtual y cualquiera que reúna las características indicadas precedentemente.
+
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <!-- NUMEROS -->
+        <div class="d-flex align-items-center mx-auto">
+          <div class="col-lg-3 col-md-12">
+            <div class="circulo-numero mx-auto my-start">
+              <h1 class="text-center text-white">3</h1>
+            </div>
+          </div>
+        </div>
+        <!-- CONDICIONES -->
+        <div class="col-lg-9 col-md-12 mx-auto mt-5">
+          <p>Para que sea considerada como dictada a distancia se requiere que la cantidad de horas no presenciales supere el 40% de la carga horaria total prevista en el respectivo plan de estudios. El porcentual se aplicará sobre la carga horaria total de la carrera sin incluir las horas correspondientes al desarrollo del trabajo ﬁnal. Los cursos en las que la cantidad de horas no presenciales se encontrara entre el 30% y el 50% del total, deberán someter a evaluación el Sistema Institucional de Educación a Distancia.
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <!-- NUMEROS -->
+        <div class="d-flex align-items-center mx-auto">
+          <div class="col-lg-3 col-md-12">
+            <div class="circulo-numero mx-auto my-start">
+              <h1 class="text-center text-white">4</h1>
+            </div>
+          </div>
+        </div>
+        <!-- CONDICIONES -->
+        <div class="col-lg-9 col-md-12 mx-auto mt-5">
+          <p>Los tutores/docentes están disponibles en la plataforma para apoyo intelectual o cualquier material necesario para su comprensión, acordara individualmente con el usuario el horario o envío de la información a través de mail, WhatsApp o lo que creen conveniente con el ﬁn de obtener los resultados necesarios para avanzar en sus objetivos.
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <!-- NUMEROS -->
+        <div class="d-flex align-items-center mx-auto">
+          <div class="col-lg-3 col-md-12">
+            <div class="circulo-numero mx-auto my-start">
+              <h1 class="text-center text-white">5</h1>
+            </div>
+          </div>
+        </div>
+        <!-- CONDICIONES -->
+        <div class="col-lg-9 col-md-12 mx-auto mt-5">
+          <p>Los exámenes están disponibles en la plataforma a partir de los siete días de habilitada la plataforma siendo los mismos de forma cronológica y organizada para poder cumplir las pautas de aprobación. Asimismo el examen ﬁnal solo podrá solicitarlo cuando tenga aprobado todos los prácticos previos. Aprobado este se le enviara por mail el resultado del mismo con el certiﬁcado correspondiente.
+          </p>
+        </div>
+      </div>
+
+      <!--ARTICULOS-->
+      <div class="mt-10">
+
+      </div>
+      <div class="row justify-content-center articulos">
+        <h1 class="Lalezar text-white">Articulo 4</h1>
+        <p>Información. El proveedor está obligado a suministrar al consumidor en forma cierta, clara y detallada todo lo relacionado con las características esenciales de los bienes y servicios que provee, y las condiciones de su comercialización. La información debe ser siempre gratuita para el consumidor y proporcionada con claridad necesaria que permita su comprensión.
+        </p>
+      </div>
+      <div class="row justify-content-center">
+        <h1 class="Lalezar text-white">Articulo 10</h1>
+        <p>Contenido del documento de venta. En el documento que se extienda por la venta de cosas muebles o inmuebles, sin perjuicio de la información exigida por otras leyes o normas, deberá constar:
+
+          a) La descripción y especiﬁcación del bien.
+          <br>b) Nombre y domicilio del vendedor.
+          <br>c) Nombre y domicilio del fabricante, distribuidor o importador cuando correspondiere.
+          <br>d) La mención de las características de la garantía conforme a lo establecido en esta ley.
+          <br>e) Plazos y condiciones de entrega.
+          <br>f) El precio y condiciones de pago.
+          <br>g) Los costos adicionales, especiﬁcando precio ﬁnal a pagar por el adquirente.<br>
+
+          La redacción debe ser hecha en idioma castellano, en forma completa, clara y fácilmente legible, sin reenvíos a textos o documentos que no se entreguen previa o simultáneamente. Cuando se incluyan cláusulas adicionales a las aquí indicadas o exigibles en virtud de lo previsto en esta ley, aquellas deberán ser escritas en letra destacada y suscritas por ambas partes. Deben redactarse tantos ejemplares como partes integren la relación contractual y suscribirse a un solo efecto. Un ejemplar original debe ser entregado al consumidor. La reglamentación establecerá modalidades más simples cuando la índole del bien objeto de la contratación así lo determine, siempre que asegure la ﬁnalidad perseguida en esta ley.
+        </p>
+      </div>
+    </div>
+  </div>
+
+<div class="container">
+  <div class="row mt-5 mb-5">
+    <div class="col">
+      <div class="caja-civil mx-auto w-75 my-25">
+        <div class="text-center text-white">
+          <p>Los sistemas de cobros de cursos a distancia estan regulados por el nuevo Codigo Civil y Comercial de la Nación en conjunto con La Ley 24.240 y su modiﬁcatoria 26.361 de Defensa del Consumidor para su protección.
+            <br>Los sistemas de cobro son habitualmente por vía electrónica para protección de todos los usuarios
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col text-center">
+      <h4 class="">¿Tuviste un inconveniente con una institución?</h4>
+      <h4>Informanos sobre cualquier problema para poder detener a cualquier institución no regulada.</h4>
+      <h3 class="mt-4 mb-5">informes@cncd.com.ar</h3>
+    </div>
+  </div>
+</div>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h2>Trabajamos por y para usted</h2>
+      </div>
+      <div class="col-2">
+
+      </div>
+      <div class="col">
+        <div class="row">
+
+            <p class="mr-1">Dirección: <strong>Perú 1318</strong></p>
+
+        </div>
+          <div class="row">
+            <p>Horario: <strong>Lun. a Vier. 7 A.M. - 10 P.M</strong></p>
+          </div>
+      </div>
+    </div>
+  </div>
+</footer>
+  </body>
+</html>
